ce1dbbc9d2cea8a0ecf77663c706d8a349fe1da6
Jiaming Lu <jiaming.lu.0124@gamil.com>
2023-10-19 15:03:46 -0400

    refactored binary_search.rs

--- src/searching/binary_search.rs
+++ src/searching/binary_search.rs
@@ -1,15 +1,15 @@
-pub fn binary_search<T: Ord>(arr: &[T], x: T) -> usize {
-    if arr.is_empty() { return 0; }
+pub fn binary_search<T: Ord>(arr: &[T], x: T) -> Option<usize> {
+    if arr.is_empty() { return None; }
     
     let mut l = 0;
     let mut r = arr.len() - 1;
     
     while l < r {
         let mid = l+r >> 1;
-        if arr[mid] == x { return mid; }
+        if arr[mid] == x { return Some(mid); }
         else if arr[mid] < x { l = mid + 1; }
         else { r = mid - 1; }
     }
 
-    0
+    None
 }
