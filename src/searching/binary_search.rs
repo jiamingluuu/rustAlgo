83428e24e82483122e51343dc639fe0019c13b30
Jiaming Lu <jiaming.lu.0124@gamil.com>
2023-10-19 11:16:39 -0400

    resolve warning in binay_search.rs

--- src/searching/binary_search.rs
+++ src/searching/binary_search.rs
@@ -4,7 +4,7 @@ pub fn binary_search<T: Ord>(arr: &[T], x: T) -> usize {
     let mut l = 0;
     let mut r = arr.len() - 1;
     
-    while (l < r) {
+    while l < r {
         let mid = l+r >> 1;
         if arr[mid] == x { return mid; }
         else if arr[mid] < x { l = mid + 1; }
